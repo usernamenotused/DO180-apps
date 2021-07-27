02f55a0463c5a0c6548b9201037392ad6f540440
Student User <student@workstation.lab.example.com>
2021-07-27 11:54:08 -0400

    more nonsense.(bn)

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,3 +1,13 @@
+<html>
+<head>
+</head>
+<body style="background:blue">
 <?php
 print "Hello, World! php version is " . PHP_VERSION . "\n";
+$s=explode("this is amazing"," ");
+foreach($w in $s){
+print "$w</br>";
+}
 ?>
+</body>
+</html>
